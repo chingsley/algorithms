fd3ea60cca4bbb4d40c9477220d9db03319fce3d
chingsley <eneja.kc@gmail.com>
2021-08-19 07:22:09 +0100

    - Create and test Node class for a linked list - Implement method to add new item to a linked list

---
+++ freecodecamp_algorithm_and_data_structures/linked_list.py
@@ -0,0 +1,54 @@
+class Node:
+    """
+    An object for storing a single node of a linked list
+    it models 2 attributes: data and a link to the next node in the list
+    """
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+
+class LinkedList:
+    """
+    Singly linked list
+    """
+
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+            current = current.next_node
+        return '->'.join(nodes)
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
+
+    def add(self, data):
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node

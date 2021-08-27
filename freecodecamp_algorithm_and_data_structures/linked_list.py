d23da732281870981cca39b84b51305e887f1b80
chingsley <eneja.kc@gmail.com>
2021-08-27 10:52:54 +0100

    - Linked List: Search method - Linked List: Insert method

--- freecodecamp_algorithm_and_data_structures/linked_list.py
+++ freecodecamp_algorithm_and_data_structures/linked_list.py
@@ -52,3 +52,42 @@ class LinkedList:
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
+
+    def search(self, key):
+        """
+        Searches for the 'first' node containing data that matches the key
+        Returns the node if found. Returns None if not found
+        Takes O(n) time
+        """
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+
+        return None
+
+    def insert(self, data, index):
+        """
+        Inserts a new Node containing data at index position
+        Insertion takes O(1) time but finding the the node at
+        the insertion point takes O(n) time.
+        """
+
+        if index == 0:
+            self.add(data)
+            return
+
+        if index > 0:
+            current = self.head
+            previous = None
+            current_index = 0
+            while current_index < index and current != None:
+                previous = current
+                current = current.next_node
+                current_index += 1
+
+            new_node = Node(data)
+            new_node.next_node = current
+            previous.next_node = new_node

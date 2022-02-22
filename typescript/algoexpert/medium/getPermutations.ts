0032c9ea3b3ab07599525e63f564f707fc005631
Kingsley Eneja <eneja.kc@gmail.com>
2022-02-22 21:08:00 +0100

    - Solve the get permutations challenge with typescript, solution 1

---
+++ typescript/algoexpert/medium/getPermutations.ts
@@ -0,0 +1,28 @@
+export function getPermutations(array: number[]) {
+  const permutations = [];
+  permutationsHelper(0, array, permutations);
+  return permutations;
+}
+
+function permutationsHelper(i: number, array: number[], permutations: number[][]) {
+  if(i === array.length - 1) {
+    permutations.push(array.slice())
+  } else {
+    for(let j = i; j < array.length; j++) {
+      swap(array, i, j);
+      permutationsHelper(i + 1, array, permutations);
+      swap(array, i, j);
+    }
+  }
+}
+
+function swap(array : number[], i :number, j :number)  {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+
+// console.log(
+//   getPermutations([1, 2, 3])
+// )

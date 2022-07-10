129b47d7d7a7a41328164c930d68c5d0d2a86741
Kingsley Eneja <kingsley.eneja@getir.com>
2022-07-10 13:34:11 +0100

    - Solve challenge 'longestPeak': x4

---
+++ typescript/algoexpert/medium/longestPeak/solution1b.ts
@@ -0,0 +1,35 @@
+// O(n) time | O(1) space
+// n = length of array
+export function longestPeak(array: number[]) {
+  let longest: number = 0;
+  let i: number = 0;
+  while (i < array.length - 1) {
+    if (isPeak(i, array)) {
+      const { peakSize, endPeak } = calculatePeakSize(i, array);
+      longest = Math.max(longest, peakSize);
+      i = endPeak + 1;
+    } else {
+      i += 1;
+    }
+  }
+  return longest;
+}
+
+function isPeak(idx: number, array: number[]) {
+  return array[idx] > array[idx - 1] && array[idx] > array[idx + 1];
+}
+
+function calculatePeakSize(i: number, array: number[]): { [key: string]: number; } {
+  let j = i - 1;
+  let k = i + 1;
+  while (j - 1 >= 0 && array[j - 1] < array[j]) {
+    j -= 1;
+  }
+  while (k + 1 < array.length && array[k + 1] < array[k]) {
+    k += 1;
+  }
+  return {
+    peakSize: k - j + 1,
+    endPeak: k
+  };
+}

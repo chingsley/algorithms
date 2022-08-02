374825f4b8997997124f3b0a983a2994297e9503
Kingsley Eneja <kingsley.eneja@getir.com>
2022-08-02 06:28:55 +0100

    - Solve challenge 'stacks'

--- typescript/algoexpert/medium/sortStack/practice.ts
+++ typescript/algoexpert/medium/sortStack/practice.ts
@@ -23,4 +23,28 @@
     }
 
   }
+  {
+    // O(n^2) time | O(n) space
+    function sortStack(stack: number[]): number[] {
+      if (stack.length === 0) return stack;
+
+      const top = stack.pop()!;
+      sortStack(stack);
+      insertInSortedStack(top, stack);
+      return stack;
+    }
+
+    function insertInSortedStack(value: number, stack: number[]): void {
+      if (stack.length === 0 || value >= stack[stack.length - 1]) {
+        stack.push(value);
+        return;
+      }
+
+      const top = stack.pop()!;
+      insertInSortedStack(value, stack);
+      stack.push(top);
+    }
+  }
 }
+
+export const ___ = '___';

9a585287654d8943695f11c90958d123d0d7575c
Kingsley Eneja <kingsley.eneja@getir.com>
2022-08-02 06:44:16 +0100

    - Solve challenge 'suffix-trie'

--- typescript/algoexpert/medium/SuffixTrie/practice.ts
+++ typescript/algoexpert/medium/SuffixTrie/practice.ts
@@ -92,6 +92,56 @@
       }
     }
   }
+  {
+    interface TrieNode {
+      [key: string]: TrieNode | boolean;
+    }
+
+    // Do not edit the class below except for the
+    // populateSuffixTrieFrom and contains methods.
+    // Feel free to add new properties and methods
+    // to the class.
+    class SuffixTrie {
+      root: TrieNode;
+      endSymbol: string;
+
+      constructor(string: string) {
+        this.root = {};
+        this.endSymbol = '*';
+        this.populateSuffixTrieFrom(string);
+      }
+
+      // O(n^2) time | O(n^2) space
+      // n = length of string
+      populateSuffixTrieFrom(string: string) {
+        for (let i = 0; i < string.length; i++) {
+          let currentNode = this.root;
+          for (let j = i; j < string.length; j++) {
+            const ch = string[j];
+            if (!(ch in currentNode)) currentNode[ch] = {};
+            currentNode = currentNode[ch] as TrieNode;
+          }
+          currentNode[this.endSymbol] = true;
+        }
+      }
+
+      // O(n) time | O(n) space
+      // n = length of string
+      contains(string: string) {
+        let currentNode = this.root;
+        for (let i = 0; i < string.length; i++) {
+          if (!(string[i] in currentNode)) return false;
+          currentNode = currentNode[string[i]] as TrieNode;
+        }
+
+        return this.endSymbol in currentNode;
+      }
+    }
+    const sTree = new SuffixTrie('test');
+    console.log(
+      JSON.stringify(sTree.root)
+    );
+  }
 }
 
 export const __ = '__';

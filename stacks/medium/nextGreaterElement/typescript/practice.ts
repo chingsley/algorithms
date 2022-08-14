7e357d818ac1d3d47913037d31a49a6e3f991284
Kingsley Eneja <kingsley.eneja@getir.com>
2022-08-14 15:11:06 +0100

    - Solve challenge 'stacks'

--- stacks/medium/nextGreaterElement/typescript/practice.ts
+++ stacks/medium/nextGreaterElement/typescript/practice.ts
@@ -52,6 +52,39 @@
       return result;
     }
   }
+  {
+    function nextGreaterElement(array: number[]) {
+      const idxStack = [0];
+      const result = new Array(array.length).fill(-1);
+
+      for (let i = 1; i < (2 * array.length); i++) {
+        const idx = i % array.length;
+        while (idxStack.length > 0 && array[idx] > array[idxStack[idxStack.length - 1]]) {
+          result[idxStack.pop()!] = array[idx];
+        }
+        idxStack.push(idx);
+      }
+
+      return result;
+    }
+  }
+  {
+    // O(n) time | O(n) space
+    function nextGreaterElement(array: number[]) {
+      const result: number[] = new Array(array.length).fill(-1);
+
+      const idxStack = [0];
+      for (let i = 1; i < (2 * array.length); i++) {
+        const idx = i % array.length;
+        while (idxStack.length > 0 && array[idx] > array[idxStack[idxStack.length - 1]]) {
+          result[idxStack.pop()!] = array[idx];
+        }
+        idxStack.push(idx);
+      }
+
+      return result;
+    }
+  }
 }
 
 export const ___ = '___';

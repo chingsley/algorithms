8f2b103aeea20a8ca9e1cc7e790667b05da667e9
Kingsley Eneja <kingsley.eneja@getir.com>
2022-10-08 17:02:13 +0100

    - Solve challenge 'suffix-trie'

--- tries/medium/SuffixTrie/typescript/practice.ts
+++ tries/medium/SuffixTrie/typescript/practice.ts
@@ -20,7 +20,6 @@
 
       // O(n^2) time | o(n^2) space
       populateSuffixTrieFrom(string: string) {
-        let root = this.root;
         for (let i = 0; i < string.length; i++) {
           let root = this.root;
           for (let j = i; j < string.length; j++) {
@@ -43,6 +42,49 @@
         return this.endSymbol in root;
       }
 
+    }
+  }
+  {
+    interface TrieNode {
+      [key: string]: TrieNode | boolean;
+    }
+
+    class SuffixTrie {
+      root: TrieNode;
+      endSymbol: string;
+
+      constructor(string: string) {
+        this.root = {};
+        this.endSymbol = '*';
+        this.populateSuffixTrieFrom(string);
+      }
+
+      // O(n^2) time | O(n^2) space
+      // n = length of input string
+      populateSuffixTrieFrom(string: string) {
+        for (let i = 0; i < string.length; i++) {
+          let currentNode = this.root;
+          for (let j = i; j < string.length; j++) {
+            const ch = string[j];
+            if (!(ch in currentNode)) currentNode[ch] = {};
+            currentNode = currentNode[ch] as TrieNode;
+          }
+          currentNode[this.endSymbol] = true;
+        }
+        // console.log(JSON.stringify(this.root))
+      }
+
+      // O(m) time | O(1) space
+      // m = length of input search string
+      contains(string: string) {
+        let currentNode = this.root;
+        for (let i = 0; i < string.length; i++) {
+          const ch = string[i];
+          if (!(ch in currentNode)) return false;
+          currentNode = currentNode[ch] as TrieNode;
+        }
+        return this.endSymbol in currentNode;
+      }
     }
   }
 }

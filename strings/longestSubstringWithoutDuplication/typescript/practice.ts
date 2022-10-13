e195dc3412e743319e76cc0360c30cba73cf4bbe
Kingsley Eneja <kingsley.eneja@getir.com>
2022-10-13 22:07:43 +0100

    - Solve challenge 'longestSubstringWithoutDuplicates'

--- strings/longestSubstringWithoutDuplication/typescript/practice.ts
+++ strings/longestSubstringWithoutDuplication/typescript/practice.ts
@@ -1,7 +1,8 @@
 {
   {
-    // O(n) time | O(n) space
+    // O(n) time | O(u) space
     // n = length of the string
+    // u = no. of unique chars in string
     function longestSubstringWithoutDuplication(string: string) {
       const seen: { [key: string]: number; } = {};
       let max = [0, 0];
@@ -19,8 +20,9 @@
     }
   }
   {
-    // O(n) time | O(n) space
+    // O(n) time | O(u) space
     // n = length of the string
+    // u = no. of unique chars in string
     function longestSubstringWithoutDuplication(string: string) {
       const seen: { [key: string]: number; } = {};
       let max = [0, 0];
@@ -35,8 +37,32 @@
       }
       return string.slice(max[0], max[1]);
     }
-
   }
+  {
+    // O(n) time | O(u) space
+    // n = length of the string
+    // u = no. of unique chars in string
+    function longestSubstringWithoutDuplication(string: string) {
+      let longest = [0, 0];
+      let startIdx = 0;
+      const seen: { [key: string]: number; } = { [string[0]]: 0 };
+      for (let i = 1; i < string.length; i++) {
+        if (string[i] in seen) {
+          startIdx = Math.max(startIdx, seen[string[i]] + 1);
+        }
+        seen[string[i]] = i;
+        updateLongest(longest, [startIdx, i]);
+      }
+      return string.slice(longest[0], longest[1] + 1);
+    }
+
+    function updateLongest(currentLongest: number[], newRange: number[]) {
+      const [a, b] = currentLongest;
+      const [c, d] = newRange;
+      if (d - c > b - a) [currentLongest[0], currentLongest[1]] = [c, d];
+    }
+  }
+
 }
 
 export const __ = '__';

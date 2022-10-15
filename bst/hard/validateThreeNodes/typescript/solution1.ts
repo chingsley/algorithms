acd1588a937aad2d100f07791c1f358e576573c0
Kingsley Eneja <kingsley.eneja@getir.com>
2022-10-15 17:19:43 +0100

    - Solve challenge: hard bst questions

---
+++ bst/hard/validateThreeNodes/typescript/solution1.ts
@@ -0,0 +1,45 @@
+// O(log(n)) time | O(1) space;
+export function validateThreeNodes(nodeOne: BST, nodeTwo: BST, nodeThree: BST) {
+  let ancestor: BST | null = null;
+  let node = findDescendantNode(nodeTwo, nodeOne);
+  if (node !== null) {
+    ancestor = nodeThree;
+  } else {
+    node = findDescendantNode(nodeTwo, nodeThree);
+    if (node !== null) ancestor = nodeOne;
+  }
+
+  if (ancestor === null) return false;
+
+  node = findDescendantNode(ancestor, nodeTwo);
+  if (node === null) return false;
+
+  return true;
+}
+
+function findDescendantNode(ancestor: BST, descendant: BST): BST | null {
+  let currNode: BST | null = ancestor;
+  while (currNode !== null) {
+    if (currNode.value === descendant.value) {
+      return currNode;
+    } else if (descendant.value < currNode.value) {
+      currNode = currNode.left;
+    } else {
+      currNode = currNode.right;
+    }
+  }
+
+  return null;
+}
+
+
+class BST {
+  value: number;
+  left: BST | null;
+  right: BST | null;
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}

c8e9632db2cef20f8c0f1576b353b17bee458d8a
Kingsley Eneja <kingsley.eneja@getir.com>
2022-12-09 15:49:18 +0100

    - Solve challenge 'howConstruct' using dynamic programming

---
+++ dynamicProgramming/structy.net/howContruct/typescript/solution1.ts
@@ -0,0 +1,18 @@
+// O(w^t) time | O(t*w) space
+export function howConstruct(target: string, words: string[], idx: number = 0): string[] | null {
+  if (idx === target.length) return [];
+
+  for (let word of words) {
+    if (word !== target.slice(idx, idx + word.length)) continue;
+
+    const res = howConstruct(target, words, idx + word.length);
+    if (res === null) continue;
+
+    return [word, ...res];
+  }
+
+  return null;
+}
+
+console.log(howConstruct('abcd', ['ab', 'abc', 'cd', 'def', 'abcd']));
+console.log(howConstruct('abcdef', ['abc', 'def']));

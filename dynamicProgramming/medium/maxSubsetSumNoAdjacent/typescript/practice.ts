a48b9c39d8ab5eaefea120926dee056cefba02fb
Kingsley Eneja <kingsley.eneja@getir.com>
2022-12-31 08:48:54 +0100

    Solve dynamic programming 'Maximum subset sum without adding adjacents'

--- dynamicProgramming/medium/maxSubsetSumNoAdjacent/typescript/practice.ts
+++ dynamicProgramming/medium/maxSubsetSumNoAdjacent/typescript/practice.ts
@@ -131,6 +131,49 @@
       return maxAt[array.length - 1];
     }
   }
+  {
+    // O(n) time | O(1) space
+    function maxSubsetSumNoAdjacent(array: number[]) {
+      if (array.length < 1) return 0;
+      if (array.length === 1) return array[0];
+
+      const maxSums = [array[0], Math.max(array[0], array[1])];
+      for (let i = 2; i < array.length; i++) {
+        const nextMaxSum = Math.max(maxSums[1], array[i] + maxSums[0]);
+        maxSums[0] = maxSums[1];
+        maxSums[1] = nextMaxSum;
+      }
+      return maxSums[1];
+    }
+  }
+  {
+    // O(n) time | O(n) space
+    function maxSubsetSumNoAdjacent(array: number[]) {
+      if (array.length < 2) return array[0] || 0;
+
+      const maxSums = [...array];
+      for (let i = 1; i < maxSums.length; i++) {
+        maxSums[i] = Math.max(maxSums[i - 1], maxSums[i] + (maxSums[i - 2] || 0));
+      }
+
+      return maxSums[maxSums.length - 1];
+    }
+  }
+  {
+    // O(n) time | O(1) space
+    function maxSubsetSumNoAdjacent(array: number[]) {
+      if (array.length < 2) return array[0] || 0;
+
+      const maxSums = [array[0], Math.max(array[0], array[1])];
+      for (let i = 2; i < array.length; i++) {
+        const nextMaxSum = Math.max(maxSums[1], array[i] + maxSums[0]);
+        [maxSums[0], maxSums[1]] = [maxSums[1], nextMaxSum];
+      }
+
+      return maxSums[1];
+    }
+
+  }
 }
 
 export const ___ = '___';

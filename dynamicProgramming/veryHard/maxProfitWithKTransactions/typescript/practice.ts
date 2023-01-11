0d524b7ff1481f7d05818b3253fc202db3242b3a
Kingsley Eneja <kingsley.eneja@getir.com>
2023-01-11 20:20:16 +0100

    Solve dynamic programming 'Max Profit With K Transactions'

---
+++ dynamicProgramming/veryHard/maxProfitWithKTransactions/typescript/practice.ts
@@ -0,0 +1,27 @@
+{
+  {
+    // O(nk) time | O(k) space (n = length of prices array)
+    function maxProfitWithKTransactions(prices: number[], k: number) {
+      const profits = new Array(2 * k).fill(0).map(
+        (_, idx) => idx % 2 === 0 ? -Infinity : 0
+      );
+
+      for (let price of prices) {
+        for (let j = 0; j < profits.length; j++) {
+          if (j === 0) {
+            profits[j] = Math.max(profits[j], -price);
+          } else if (j % 2 === 0) {
+            profits[j] = Math.max(profits[j], profits[j - 1] - price);
+          } else {
+            profits[j] = Math.max(profits[j], profits[j - 1] + price);
+          }
+        }
+      }
+
+
+      return profits[profits.length - 1];
+    }
+  }
+}
+
+export const __ = '__';

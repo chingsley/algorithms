61036f5876bf7fbd715b1cae8cd809ef644f0714
Kingsley Eneja <kingsley.eneja@getir.com>
2023-07-29 23:25:05 +0100

    Solve binary tree 'maxPathSum'

--- binaryTree/hard/maxPathSum/typescript/practice.ts
+++ binaryTree/hard/maxPathSum/typescript/practice.ts
@@ -42,4 +42,47 @@ export class BinaryTree {
       max: number;
     };
   }
+  {
+    class BinaryTree {
+      value: number;
+      left: BinaryTree | null;
+      right: BinaryTree | null;
+
+      constructor(value: number) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+      }
+    }
+
+    // O(n) time | O(d) space (n = no. of nodes, d = depth of tree)
+    function maxPathSum(tree: BinaryTree) {
+      const [currMaxPathSum, _] = maxPathSumHelper(tree);
+      return currMaxPathSum;
+    }
+
+    function maxPathSumHelper(node: BinaryTree | null): [number, number] {
+      if (node === null) return [-Infinity, -Infinity];
+
+      if (node.left === null && node.right === null) {
+        return [node.value, node.value];
+      }
+
+      const [leftMaxSum, leftMaxPathSum] = maxPathSumHelper(node.left);
+      const [rightMaxSum, rightMaxPathSum] = maxPathSumHelper(node.right);
+      const currMaxSum = Math.max(
+        node.value,
+        node.value + leftMaxPathSum,
+        node.value + rightMaxPathSum,
+        node.value + leftMaxPathSum + rightMaxPathSum,
+        leftMaxSum,
+        rightMaxSum
+      );
+      return [
+        currMaxSum,
+        Math.max(node.value + leftMaxPathSum, node.value + rightMaxPathSum) // runningMaxPathSum
+      ];
+    }
+
+  }
 }
